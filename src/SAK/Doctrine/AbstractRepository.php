4d9529824a7516b2e60c7ef78190adcf0292aeee
Luis Silva <luis.c.silva.info@gmail.com>
2019-12-18 12:09:35 -0300

    Atualização para salvar varias linhas do mesmo arquivo na importação

--- src/SAK/Doctrine/AbstractRepository.php
+++ src/SAK/Doctrine/AbstractRepository.php
@@ -15,52 +15,65 @@ abstract class AbstractRepository extends \Doctrine\ORM\EntityRepository {
     protected $path_entity_default = '%s\Entity\%s';
 
     protected function getFileNameReturn($pathDownload) {
-		$file_name_return = 'retorno_upload_' . date('YmdHi') . '_' . rand(1, 1000) . '.csv';
-		$file_return = $pathDownload . $file_name_return;
-		return [ 'file_name_return' => $file_name_return, 'file_return' => $file_return ];
-	}
+        $file_name_return = 'retorno_upload_' . date('YmdHi') . '_' . rand(1, 1000) . '.csv';
+        $file_return = $pathDownload . $file_name_return;
+        return [ 'file_name_return' => $file_name_return, 'file_return' => $file_return ];
+    }
 
-	public function importFromFile($fileToImport, $pathDownload) {
-		list($file_name_return, $file_return) = array_values($this->getFileNameReturn($pathDownload));
-		$oReturnFile = fopen($file_return, 'w');
+    public function importFromFile($fileToImport, $pathDownload) {
+        list($file_name_return, $file_return) = array_values($this->getFileNameReturn($pathDownload));
+        $oReturnFile = fopen($file_return, 'w');
 
-		$adapter = new ImportAdapter($this->getFieldAdapter());
+        $adapter = new ImportAdapter($this->getFieldAdapter());
 
-		$rows = $adapter->importFile($fileToImport);
-		$this->importInsertLines($rows, $oReturnFile);
-		$status = $adapter->getContinueProcessStatus();
-		while($status) {
-   		    $rows = $adapter->process();
-		    $this->importInsertLines($rows, $oReturnFile);
-		    $status = $adapter->getContinueProcessStatus();
-		}
+        $rows = $adapter->importFile($fileToImport);
+        $this->importInsertLines($rows, $oReturnFile);
+        $status = $adapter->getContinueProcessStatus();
+        while($status) {
+               $rows = $adapter->process();
+            $this->importInsertLines($rows, $oReturnFile);
+            $status = $adapter->getContinueProcessStatus();
+        }
 
-	    fclose($oReturnFile);
+        fclose($oReturnFile);
 
-	    return ['file_name' => $file_name_return];
-	}
+        return ['file_name' => $file_name_return];
+    }
 
-	public function importInsertLines($rows, $fileReturn) {
-		foreach($rows as $row) {
-	        try {
-	        	// Prepara o objeto que deverá ser inserido e faz algumas validações basicas
-	        	$aCreate = $this->prepareLineToImport($row);
+    public function importInsertLines($rows, $fileReturn) {
+        foreach($rows as $row) {
+            try {
+                // Prepara o objeto que deverá ser inserido e faz algumas validações basicas
+                $cloneObj = clone $this;
+                $aCreate = $cloneObj->prepareLineToImport($row);
 
-	        	$message = $aCreate['message'];
-	        	if ($aCreate['status']) {
-	        		$this->create($aCreate['object']);
-	        		$message = 'SUCESSO';
-	        	}
-	        } catch (\Exception $e) {
-	        	$message = $e->getMessage();
-	        } finally {
-	        	$row->message = $message;
-	        	fputcsv($fileReturn, (array) $row);
-	        }
-		}
+                $message = $aCreate['message'];
+                if ($aCreate['status']) {
+                    $cloneObj->create($aCreate['object']);
+                    $message = 'SUCESSO';
+                }
+            } catch (\Exception $e) {
+                $message = $e->getMessage();
 
-	    return true;
-	}
+                /**
+                 * @see
+                 * Quando ocorre um erro no insert é exibido um erro 'The EntityManager is closed Doctrine'
+                 * as linhas abaixo reabrem a conexão quando evitando esse erro
+                 */
+                if (!$this->_em->isOpen()) {
+                    $this->_em = $this->_em->create(
+                        $this->_em->getConnection(),
+                        $this->_em->getConfiguration()
+                    );
+                }
+            } finally {
+                $row->message = $message;
+                fputcsv($fileReturn, (array) $row);
+            }
+        }
+
+        return true;
+    }
 
     public function getPaginateInfo() {
         return $this->objPaginate->getPaginateInfo();
